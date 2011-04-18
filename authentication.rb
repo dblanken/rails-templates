865e2968a3c3cecffc7230871633403e6ccf4b42
David Blankenship <dblanken@iupui.edu>
2011-04-18 13:23:44 -0400

    Added rails3 branch

--- authentication.rb
+++ authentication.rb
@@ -1,6 +1,6 @@
 apply "https://github.com/dblanken/rails-templates/raw/master/base.rb"
 
-plugin 'rubycas-client-iupui', :git => 'ssh://deploy@cry/var/git/rubycas-client-iupui.git', :branch => "rails3"
+plugin 'rubycas-client-iupui', :git => '-r rails3 ssh://deploy@cry/var/git/rubycas-client-iupui.git'
 generate :iupui_cas
 rake "db:migrate"
 
